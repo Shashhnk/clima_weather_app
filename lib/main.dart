1405833c93966899c46315f8506dac7acdc3e58d
shashanksf <94291816+shashanksf@users.noreply.github.com>
2023-06-26 19:43:28 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-
-
-import 'package:clima_weather_app/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/Loadingscreen.dart';
 
@@ -10,17 +7,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home:  LoadingScreen(),
+      home: const LoadingScreen(),
     );
   }
 }
